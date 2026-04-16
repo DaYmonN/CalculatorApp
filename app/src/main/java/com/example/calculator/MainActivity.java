7c6f627271e369343df120ebd83c67cb9ab7ff63
DaYmonN <darmondik@gmail.com>
2026-04-16 19:41:02 +0300

    Реализован метод onCreate с инициализацией виджетов

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -1,12 +1,39 @@
 package com.example.calculator;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    TextView textResult;
+    String currentNumber = "";
+    String lastOperation = "";
+    double firstValue = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        textResult = findViewById(R.id.textResult);
+        
+        // Инициализация кнопок (цифры и операции будут в другой ветке)
+        Button btn0 = findViewById(R.id.btn0);
+        Button btn1 = findViewById(R.id.btn1);
+        Button btn2 = findViewById(R.id.btn2);
+        Button btn3 = findViewById(R.id.btn3);
+        Button btn4 = findViewById(R.id.btn4);
+        Button btn5 = findViewById(R.id.btn5);
+        Button btn6 = findViewById(R.id.btn6);
+        Button btn7 = findViewById(R.id.btn7);
+        Button btn8 = findViewById(R.id.btn8);
+        Button btn9 = findViewById(R.id.btn9);
+        Button btnAdd = findViewById(R.id.btnAdd);
+        Button btnSub = findViewById(R.id.btnSub);
+        Button btnMul = findViewById(R.id.btnMul);
+        Button btnDiv = findViewById(R.id.btnDiv);
+        Button btnEq = findViewById(R.id.btnEq);
+        Button btnClear = findViewById(R.id.btnClear);
     }
 }
